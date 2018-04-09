1d7e333bef9e991320a88e5021c256d6aa8b42fe
celahm <34541727+celahm@users.noreply.github.com>
2018-04-09 20:44:31 +0200

    Add strings / Add Student linear layout
    Add strings / Add Student linear layout

--- app/src/main/java/com/schoolapp/schoolapp/AddStudent.java
+++ app/src/main/java/com/schoolapp/schoolapp/AddStudent.java
@@ -2,6 +2,8 @@ package com.schoolapp.schoolapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AddStudent extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class AddStudent extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_student);
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.all_classes);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.all_classes, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+/* Apply the adapter to the spinner */
+        spinner.setAdapter(adapter);
     }
+
+
+
+
 }
